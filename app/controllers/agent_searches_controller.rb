2d5905d40ad57d4b05c0e35f2e9fb251071048b9
Javid Jamae <javidjamae@gmail.com>
2017-11-19 17:34:30 -0800

    added a background image, logo, and basic formatting to search page

--- app/controllers/agent_searches_controller.rb
+++ app/controllers/agent_searches_controller.rb
@@ -7,12 +7,12 @@ class AgentSearchesController < ApplicationController
 
   def create
     search = AgentSearch.new(params.permit(:txn_side, :prop_type, :price_range))
-    
+
     if search.txn_side.blank? || search.prop_type.blank? || search.price_range.blank?
       redirect_to(root_path)
       return
     end
-    
+
     search.find_agent_matches!
 
     if search.save
